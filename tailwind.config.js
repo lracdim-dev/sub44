629f345b1a703119dae28f2acb819910a312912c
smcc-carl <carl.smcc24@gmail.com>
2024-10-08 05:38:25 +0000

    responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js,njk,md}"],
   theme: {
+    theme: {
+      screens: {
+        'mobile': '320px',
+        // => @media (min-width: 320px) { ... }
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
     extend: {
       container:{
         center:true,
-       
-      }
+      },
     },
   },
   plugins: [
